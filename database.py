fe28840cb0d732d03fa73da65ee1757870d235f3
George Powell <georgejpowell51@gmail.com>
2024-01-07 00:04:01 +0000

    WIP database interaction

--- database.py
+++ database.py
@@ -5,23 +5,29 @@ from manager import Manager
 import sqlalchemy
 
 
-def write_to_db():
+def init_cnx():
 
-    """Writing to fpl_model database."""
+    """Initialize database connection."""
 
     load_dotenv()
-
-    # me = Manager(os.environ.get('ME'))
-    # print([(i.ownership, i.second_name) for i in me.current_team.get_players()])
-
     engine = sqlalchemy.create_engine(f'mysql://app:{os.environ.get('DB_PASS')}@localhost/fpl_model')
-    conn = engine.connect()
+    cnx = engine.connect()
 
-    squad_df = pd.read_csv('team_strengths.csv', sep=',', header=0)
-    squad_df.to_sql('squad_gameweek', con=conn, if_exists='append', index=False)
-
-    conn.close()
+    return cnx
 
 
-if __name__ == '__main__':
-    write_to_db()
+# def write_to_db():
+
+#     """Writing to fpl_model database."""
+
+#     # me = Manager(os.environ.get('ME'))
+#     # print([(i.ownership, i.second_name) for i in me.current_team.get_players()])
+
+#     squad_df = pd.read_csv('team_strengths.csv', sep=',', header=0)
+#     squad_df.to_sql('squad_gameweek', con=cnx, if_exists='append', index=False)
+
+#     cnx.close()
+
+
+# if __name__ == '__main__':
+#     write_to_db()
